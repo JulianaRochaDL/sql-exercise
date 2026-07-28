2263e324bb549d8b3d332e4de0314cc30cf8cbb8
Juliana <juliana.rocha.dl@gmail.com>
2026-07-28 00:07:53 -0300

    Create hackerrank_aggregation_02.sql

---
+++ HackerRank/hackerrank_aggregation_02.sql
@@ -0,0 +1,7 @@
+-- Primeiro exercicio de Aggregation realizado no RackerRank, onde foi solicitado o seguinte select: Query the total population of all cities in CITY where District is California.
+
+-- Language: MySQL
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
