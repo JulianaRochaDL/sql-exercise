63dfb5a7cef1a9e88e3d9b1b26c0635caa282a92
Juliana <juliana.rocha.dl@gmail.com>
2026-07-27 23:12:55 -0300

    Editando comentário

--- HackerRank/hackerrank_advanced_select_02.sql
+++ HackerRank/hackerrank_advanced_select_02.sql
@@ -1,4 +1,4 @@
--- Primeiro exercicio avançado realizado no RackerRank, onde foi solicitado o seguinte select:
+-- Segundo exercicio avançado realizado no RackerRank, onde foi solicitado o seguinte select:
 -- 1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
 -- 2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
 -- There are a total of [occupation_count] [occupation]s.
