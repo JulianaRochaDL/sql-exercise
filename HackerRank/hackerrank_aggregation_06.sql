2c2232d2dee5e4a6e6d46ae9424530193a1a88a8
Juliana <juliana.rocha.dl@gmail.com>
2026-08-05 22:01:46 -0300

    Create hackerrank_aggregation_06.sql

---
+++ HackerRank/hackerrank_aggregation_06.sql
@@ -0,0 +1,6 @@
+-- Quinto exercicio de Aggregation realizado no RackerRank, onde foi solicitado o seguinte select: Query the difference between the maximum and minimum populations in CITY.
+
+-- Language: MySQL
+
+SELECT MAX(Population) - MIN(Population)
+FROM CITY;
