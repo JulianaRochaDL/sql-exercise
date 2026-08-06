a6dec2b95adc23e6ef8487c604445d066fa6b95c
Juliana <juliana.rocha.dl@gmail.com>
2026-08-05 22:15:55 -0300

    Alteração no comentário

--- HackerRank/hackerrank_aggregation_06.sql
+++ HackerRank/hackerrank_aggregation_06.sql
@@ -1,4 +1,4 @@
--- Quinto exercicio de Aggregation realizado no RackerRank, onde foi solicitado o seguinte select: Query the difference between the maximum and minimum populations in CITY.
+-- Sexto exercicio de Aggregation realizado no RackerRank, onde foi solicitado o seguinte select: Query the difference between the maximum and minimum populations in CITY.
 
 -- Language: MySQL
 
